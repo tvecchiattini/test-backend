04c822b06ab1149e128b49945615c49390c0fb48
Tommaso Vecchiattini <tommaso.vecchiattini@abstract.it>
2023-11-01 13:51:41 +0100

    new field

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,9 +4,16 @@ export const TUser = Type.Object({
     _id: Type.Optional(Type.String()),
     firstname: Type.String(),
     lastname: Type.String(),
-    email: Type.String(),
-    country: Type.String(),
-    state: Type.String(),
-})
+    email: Type.String({
+        format: 'email'
+    }),
+    country: Type.String({
+        maxLength: 2
+    }),
+    state: Type.String({
+        maxLength: 2
+    }),
+    age: Type.Optional(Type.Number()),
+}, { additionalProperties: false })
 
 export type User = Static<typeof TUser>
